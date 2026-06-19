acf480fa6eb2bd778ee06a0966d3e6553b1076ec
César Pichon <cesarpichon@gmail.com>
2026-06-19 17:59:13 +0200

    Add evaluation

---
+++ evaluation/evaluation.py
@@ -0,0 +1,26 @@
+import argparse
+
+def parlement():
+    pass
+
+def catie_sts():
+    pass
+
+def sick():
+    pass
+
+def medialab():
+    pass
+
+def embeddings():
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "model",
+        help="Model to evaluate"
+    )
+    args = parser.parse_args()
+
+    pass
