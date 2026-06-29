144e16bc5eaba0ee3cdeecc805912b38dc8c6520
César Pichon <cesarpichon@gmail.com>
2026-06-29 13:26:25 +0200

    remove fix

--- training/train.py
+++ training/train.py
@@ -301,7 +301,6 @@ def trial(
         gradient_checkpointing_kwargs={"use_reentrant": False},
         gradient_accumulation_steps=accumumation_steps,
         dataloader_drop_last=True,
-        preprocess_logits_for_metrics=lambda logits, _: torch.argmax(logits, dim=-1),
         seed=SEED,
     )
 
