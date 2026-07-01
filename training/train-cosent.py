211616eb66c42e337fec4d1760930dcf0a4e452d
César Pichon <cesarpichon@gmail.com>
2026-07-01 18:00:33 +0200

    fix

--- training/train-cosent.py
+++ training/train-cosent.py
@@ -332,6 +332,7 @@ def trial(
         activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
         record_shapes=True,
         profile_memory=True,
+        schedule=torch.profiler.schedule(active=5),
         on_trace_ready=trace_handler,
         acc_events=True,
     ) as prof:
